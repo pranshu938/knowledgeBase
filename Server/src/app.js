867b10e42c1f56cc8241056dbbcc7dac72fa8c10
Pranshu Kumar <pranshu@Pranshus-MacBook-Air.local>
2026-05-10 19:00:14 +0530

    cors changes

--- Server/src/app.js
+++ Server/src/app.js
@@ -16,7 +16,7 @@ app.use(helmet()); // security headers
 app.use(compression()); // gzip
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: process.env.CLIENT_URL, // frontend URL
     credentials: true,
   })
 );
